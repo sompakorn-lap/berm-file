05a5b369fe2db83d60aa6230b62dbd7748303020
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-01-25 12:57:38 +0700

    refac

--- server/src/libs/mongoose/connectDatabase.ts
+++ server/src/libs/mongoose/connectDatabase.ts
@@ -1,11 +1,12 @@
 import { connect } from "mongoose";
 
 async function connectDatabase() {
-	try {
+  try {
     await connect(process.env.MONGODB_URI as string);
     console.log("connect mongodb successfully.");
+  } catch (err) {
+    console.error("[ERROR]: failed to connect mongodb.", err);
   }
-  catch (err) { console.error("[ERROR]: failed to connect mongodb.", err); }
 }
 
 export default connectDatabase;
