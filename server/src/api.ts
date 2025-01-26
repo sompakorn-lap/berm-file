0be0b3e1790a4f865fd0554d4cc31120e1766cff
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-01-26 17:36:44 +0700

    feat(server): apply folder api

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,5 +1,6 @@
 import Elysia from "elysia";
 import fileApi from "./features/file/api";
+import folderApi from "./features/folder/api";
 
-const api = new Elysia({ prefix: "/api" }).use(fileApi);
+const api = new Elysia({ prefix: "/api" }).use(fileApi).use(folderApi);
 export default api;
