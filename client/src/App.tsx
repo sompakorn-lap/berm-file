584f8143f1772e7c495463ec20d82b4ef2a6b1c3
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-01-26 17:46:36 +0700

    refac(client): cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Home.page";
 import { Icon } from "@iconify/react";
 import ReactQueryProvider from "./libs/react-query/ReactQueryProvider";
-import UserPage from "./features/user/container/UserPage";
 
 function App() {
   return (
@@ -10,14 +9,13 @@ function App() {
       <div className="grid grid-rows-[auto_1fr_auto] min-h-screen w-screen">
 
         <header className="w-full p-4 text-center bg-black text-gray-100">
-          <h1 className="text-xl">BomSLap - BERM stack</h1>
+          <h1 className="text-xl">BERM stack - File system</h1>
         </header>
 
         <main className="p-2">
           <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage/>} />
-                <Route path="/user" element={<UserPage/>} />
             </Routes>
           </BrowserRouter>
         </main>
@@ -25,7 +23,7 @@ function App() {
         <footer className="w-full p-2 bg-black text-gray-100">
           <a
             className="flex gap-2"
-            href="https://github.com/sompakorn-lap/basic-setup-berm-stack"
+            href="https://github.com/sompakorn-lap/berm-file"
           >
             <Icon icon="mdi:github" width={24} height={24} />
             <span>GITHUB</span>
